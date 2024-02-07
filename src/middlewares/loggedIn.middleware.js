4549d6a7dc0e9c350909516aaac592e70497d9f6
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-02-07 14:23:10 +0100

    seperate middleware to new directory structure

---
+++ src/middlewares/loggedIn.middleware.js
@@ -0,0 +1,7 @@
+export default function isLoggedIn(req, res, next) {
+  if (req.session.user) {
+    res.redirect('/panel'); // Redirect to login page if not authenticated
+  } else {
+    next();
+  }
+}
