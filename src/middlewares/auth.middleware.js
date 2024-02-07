9daf31594c045d25f144bbc9ed8c461d5ed49a6e
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-02-07 14:23:18 +0100

    seperate middleware to new directory structure

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,7 @@
+export default function isAuthenticated(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login'); // Redirect to login page if not authenticated
+  }
+}
