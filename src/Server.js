9e3715f45401cd86abb68029a1274c02949de1b3
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-02-14 15:17:55 +0100

    add routes: panel,stream,recorder,login,logout

--- src/Server.js
+++ src/Server.js
@@ -1,14 +1,25 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import helmet from "helmet";
-import session from "express-session";
-import rateLimit from "express-rate-limit";
-import { serverConfig } from "./config/config.js";
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require("body-parser")
+const helmet = require("helmet")
+const session = require("express-session")
+const rateLimit = require("express-rate-limit")
+const { serverConfig } = require("./config/config.js")
+const panelRoutes = require("./routes/panel.routes.js")
+const streamRoutes = require("./routes/stream.routes.js")
+const recorderRoutes = require('./routes/recorder.routes.js')
+const logoutRoutes = require('./routes/logout.routes.js')
+const loginRoutes = require('./routes/login.routes.js')
 
-export default class Server {
+class Server {
   constructor(app) {
     this.config(app);
+
+    app.use('/', panelRoutes);
+    app.use('/', streamRoutes);
+    app.use('/', recorderRoutes);
+    app.use('/', logoutRoutes)
+    app.use('/', loginRoutes)
   }
 
   config(app) {
@@ -25,3 +36,5 @@ export default class Server {
     app.use(express.static('public'));
   }
 }
+
+module.exports = Server 
