66f46437dd35648eff70e3c85d357294dc75aada
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-02-14 15:25:42 +0100

    add module.exports import

--- src/middlewares/loggedIn.middleware.js
+++ src/middlewares/loggedIn.middleware.js
@@ -1,4 +1,4 @@
-export default function isLoggedIn(req, res, next) {
+module.exports = function isLoggedIn(req, res, next) {
   if (req.session.user) {
     res.redirect('/panel'); // Redirect to login page if not authenticated
   } else {
