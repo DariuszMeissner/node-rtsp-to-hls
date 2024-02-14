775294188b567bb521e6853aa9aff405b781364d
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-02-14 15:27:56 +0100

    move route to streamRoutes class

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const controller = require('../controllers/stream')
-
-const router = Router();
-
-router.get('/start-stream', controller.startStream)
-
-module.exports = router
