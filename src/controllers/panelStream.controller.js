807a080189a3cb0f2348de00326a14b3e809a7e0
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-02-16 15:02:13 +0100

    fixed path of panelStream controller

--- src/controllers/panelStream.controller.js
+++ src/controllers/panelStream.controller.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const panelStream = (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pages', 'panel-admin.html'));
+  res.sendFile(path.join(__dirname, '../../public/pages', 'panel-admin.html'));
 }
 
 module.exports = panelStream 
