4afd696c4ab3bb21d6254061a8e3eb921daa47bd
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-03-03 20:46:28 +0100

    chnaged logic api

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,4 +1,4 @@
-const path = require('path')
+// const path = require('path')
 
 const loginPostController = async (req, res) => {
   try {
@@ -6,7 +6,8 @@ const loginPostController = async (req, res) => {
 
     if (username === process.env.USER_LOGIN && password === process.env.USER_PASSWORD) {
       req.session.user = { username: process.env.USER_LOGIN }; // Set user in session
-      res.json({ success: true, redirect: '/panel' });
+      res.json({ success: true, message: 'Credentials correct' });
+      console.log('session created');
     } else {
       res.json({ success: false, message: 'Invalid credentials' });
     }
@@ -15,11 +16,11 @@ const loginPostController = async (req, res) => {
   }
 }
 
-const loginGetController = async (req, res) => {
-  try {
-    res.sendFile(path.join(__dirname, '../../public/pages', 'login-panel.html'));
-  } catch (error) {
-    console.error(error);
+const loginGetController = (req, res) => {
+  if (req.session.user) {
+    res.json({ logged: true })
+  } else {
+    res.json({ logged: false })
   }
 }
 
