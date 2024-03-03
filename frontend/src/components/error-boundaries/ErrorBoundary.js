9e3c9d4e0c27b69845c23cb283c118a2d65d894a
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-03-03 20:37:35 +0100

    created error boundary component

---
+++ frontend/src/components/error-boundaries/ErrorBoundary.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  fallback: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+
+
+export default ErrorBoundary;
