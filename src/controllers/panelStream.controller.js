7acaa405f9932b138ed2aa54b1d0eb160a9d3516
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-03-03 20:49:38 +0100

    send json data

--- src/controllers/panelStream.controller.js
+++ src/controllers/panelStream.controller.js
@@ -1,7 +1,12 @@
-const path = require('path')
+// const path = require('path')
 
-const panelStream = (_, res) => {
-  res.sendFile(path.join(__dirname, '../../public/pages', 'panel-admin.html'));
+const panelStream = (req, res) => {
+  // res.sendFile(path.join(__dirname, '../../public/pages', 'panel-admin.html'));
+  if (req.session.user) {
+    res.json({ logged: true })
+  } else {
+    res.json({ logged: false })
+  }
 }
 
 module.exports = panelStream 
