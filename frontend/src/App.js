fc2ac6a4efd8a01ea1fba839ac387f4dd2d6b97c
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-03-03 20:34:40 +0100

    added titles pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import AdminPanel from './pages/AdminPanel';
 import NoPage from './pages/NoPage';
+import { PAGES } from './config/pages.config';
 
 function App() {
+  const [currentPath, setCurrentPath] = useState(document.location.pathname)
+
+  function setDocumentTitle(path) {
+    switch (path) {
+      case '/login':
+        document.title = PAGES.title.login;
+        break;
+      case '/panel':
+        document.title = PAGES.title.adminPanel;
+        break;
+      case '/':
+        document.title = PAGES.title.home;
+        break;
+      default:
+        document.title = PAGES.title.noPage;
+        break;
+    }
+
+  }
+
+  useEffect(() => {
+    setCurrentPath(document.location.pathname);
+    setDocumentTitle(currentPath);
+  }, [currentPath]);
+
   return (
     <BrowserRouter>
       <Routes>
