49e35a6b92fdc3ce6b5a38104fa49d496973d33e
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-03-03 20:44:43 +0100

    catch error component

--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import AdminPanelView from '../components/admin-panel/AdminPanelView'
+import ErrorBoundary from '../components/error-boundaries/ErrorBoundary'
+import Loading from '../components/loading/Loading';
 
 const AdminPanel = () => {
   return (
-    <div>AdminPanel</div>
+    <ErrorBoundary fallback={<div>Something went wrong..., plese try again</div>}>
+      <Suspense fallback={<Loading />}>
+        <AdminPanelView />
+      </Suspense>
+    </ErrorBoundary>
   )
 }
 
