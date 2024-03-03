c377f00a736e92ec13b5fdc85ccd4d23a542a74a
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-03-03 20:39:46 +0100

    added title of pages

---
+++ frontend/src/config/pages.config.js
@@ -0,0 +1,8 @@
+export const PAGES = {
+  title: {
+    adminPanel: 'Panel zarządzania transmisją',
+    home: 'Transmisja z posiedzenia Rady Powiatu Brzeziny ',
+    login: 'Logowanie do panelu admin',
+    noPage: 'Srona nie istnieje',
+  }
+}
