fe8ff9452b0bd79875d29c92870cb2cb286ee17d
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-03-13 09:23:29 +0100

    increase rate limiter on 1000

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,7 @@ const serverConfig = {
   },
   limiter: {
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    max: 1000 // limit each IP to 100 requests per windowMs
   },
   helmetOptions: {
     directives: {
