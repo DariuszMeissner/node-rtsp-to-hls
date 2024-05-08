5950f2de7971f7efc240dba2d0e603ad816f3d14
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-05-08 15:04:19 +0200

    fixed - video stream after logout is still on

--- src/controllers/logout.controller.js
+++ src/controllers/logout.controller.js
@@ -1,7 +1,3 @@
-const Stream = require('../Stream')
-const Recorder = require('../Recorder')
-
-// Helper function to destroy a session and return a promise
 const destroySession = (session) => {
   return new Promise((resolve, reject) => {
     session.destroy((err) => {
@@ -14,15 +10,8 @@ const destroySession = (session) => {
 
 const logoutPostController = async (req, res) => {
   if (req.session) {
-    const streamInstance = Stream.getInstance();
-    const recorderInstance = Recorder.getInstanceRecorder();
-
     try {
       await destroySession(req.session);
-      streamInstance.killStreamProcess();
-      recorderInstance.endRecording();
-
-      console.log(`Logout successful`);
       res.json({ success: true, redirect: '/login' })
 
     } catch (error) {
